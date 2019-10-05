b81926e8fc1d02cb297278f6be7c843828d0208d
ihsan3975 <m.ihsan3975@gmail.com>
2019-10-06 05:41:19 +0700

    Edited in middleware

--- src/components/CardProductHome.js
+++ src/components/CardProductHome.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import errorImage from "../img/errorImage.svg";
 // import API from "../axios/Api";
 
 import "react-confirm-alert/src/react-confirm-alert.css";
@@ -7,7 +8,13 @@ import "react-confirm-alert/src/react-confirm-alert.css";
 function CardProductHome({ product }) {
   return (
     <div className="col-md-12 card" style={{ margin: 2 }}>
-      <img src={product.image} />
+      <img
+        src={product.image}
+        // onError={() => {
+        //   product.image = errorImage;
+        //   forceUpdate();
+        // }}
+      />
       <h3>{product.name}</h3>
       <small>Quantity: {product.quantity}</small>
       <p>Category: {product.category}</p>
--- src/components/ListProductHome.js
+++ src/components/ListProductHome.js
@@ -34,7 +34,6 @@ export class ListProduct extends Component {
         <CardProductHome
           product={product}
           key={product.id}
-          s
           refresh={this.componentDidMount}
         />
       );
